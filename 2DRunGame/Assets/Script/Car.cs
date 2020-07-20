709e03dcb088ee63c880a8fb88d6d741c6d7bb74
eric111333 <67904763+eric111333@users.noreply.github.com>
2020-07-20 21:26:42 +0800

    2D

---
+++ 2DRunGame/Assets/Script/Car.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    //資料
+    string brand;
+    int cc;
+    float weight;
+    bool window;
+    float speed;
+    //命名規則
+    //不允許
+    //1.特殊符號2.數字開頭3.保留字
+    //允許
+    //1.底線2.數字非開頭3.中文
+
+}
---
+++ 2DRunGame/Assets/Script/First.cs
@@ -0,0 +1,13 @@
+﻿//引用using引擎
+using UnityEngine;
+
+//類別class腳本名稱
+public class First : MonoBehaviour
+{
+    //
+    //
+    //
+    //
+
+
+}
