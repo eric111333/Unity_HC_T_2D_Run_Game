f0f772d2bf790aa07ee8c5a728cd7c171df8f34b
eric111333 <67904763+eric111333@users.noreply.github.com>
2020-07-27 21:28:05 +0800

    2D

--- 2DRunGame/Assets/API/Player.cs
+++ 2DRunGame/Assets/API/Player.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
@@ -79,18 +77,13 @@ public class Player : MonoBehaviour
     #endregion
 
     #region 事件
+    private void Start()
+    {
 
+    }
+    private void Update()
+    {
+
+    }
     #endregion
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
