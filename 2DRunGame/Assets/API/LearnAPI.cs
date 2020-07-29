d8b20cf6d444aba20e6fa757b73a55127e1168f3
eric111333 <67904763+eric111333@users.noreply.github.com>
2020-07-29 21:30:58 +0800

    2D_RUN

---
+++ 2DRunGame/Assets/API/LearnAPI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    public GameObject sphere;
+    public Transform tra;
+    public Transform cube;
+    public Light light;
+
+    void Start()
+    {
+        print(sphere.layer);
+        print("球的座標" + tra.position);
+        tra.localScale = new Vector3(9, 9, 9);
+
+        
+    }
+
+    void Update()
+    {
+        cube.Rotate(0, 0, 5);
+        cube.Translate(0.5f, 0, 0);
+    }
+}
