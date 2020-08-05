fb3016ef739a4d6ba2c30b2ccbf7f7172c4b2832
eric111333 <67904763+eric111333@users.noreply.github.com>
2020-08-05 21:33:41 +0800

    0805

---
+++ 2DRunGame/Assets/API/Learn/LearnLerp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class LearnLerp : MonoBehaviour
+{
+    public float A = 0;
+    public float B = 111;
+
+    private void Start()
+    {
+        float result = Mathf.Lerp(A, B, 0.5f);
+        print(result);
+    }
+    public float C = 0;
+    public float D = 100;
+
+    public Vector2 v2A = new Vector2(0, 0);
+    public Vector2 v2B = new Vector2(1000, 1000);
+
+    private void Update()
+    {
+        C = Mathf.Lerp(C, D, 0.005f * Time.deltaTime);
+        v2A = Vector2.Lerp(v2A, v2B, 0.005f * Time.deltaTime);
+    }
+}
--- 2DRunGame/Assets/API/Player.cs
+++ 2DRunGame/Assets/API/Player.cs
@@ -3,9 +3,9 @@
 public class Player : MonoBehaviour
 {
     #region 欄位
-    [Header("移動速度"),Range(0,1000)]
+    [Header("移動速度"), Range(0, 1000)]
     public float speed = 5;
-    [Header("跳躍高度"),Range(0,1000)]
+    [Header("跳躍高度"), Range(0, 1000)]
     public int jump = 350;
     [Header("血量"), Range(0, 2000)]
     public float hp = 500;
@@ -19,8 +19,9 @@ public class Player : MonoBehaviour
     public AudioClip soundCoin;
 
     public Animator ani;
-    public Rigidbody2D rid;
+    public Rigidbody2D rig;
     public CapsuleCollider2D cap;
+    public AudioSource aud;
     #endregion
 
     #region 方法
@@ -29,33 +30,63 @@ public class Player : MonoBehaviour
     /// </summary>
     private void Move()
     {
-        
+        transform.Translate(speed*Time.deltaTime, 0, 0);
     }
     /// <summary>
     /// 跳躍
     /// </summary>
     private void Jump()
     {
-        bool space = Input.GetKeyDown(KeyCode.Space)||Input.GetKeyDown(KeyCode.UpArrow);
+        bool space = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow);
         //bool up = Input.GetKeyDown(KeyCode.UpArrow);
-        ani.SetBool("jump switch", space);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, -0.8f), -transform.up, 0.01f, 1 << 8);
+        if (hit)
+        {
+            isGround = true;
+            ani.SetBool("jump switch", false);
+        }
+        else
+        {
+            isGround = false;
+        }
+
+        if (isGround)
+        {
+            if (space)
+            {
+                ani.SetBool("jump switch", true);
+                rig.AddForce(new Vector2(0, jump));
+                aud.PlayOneShot(soundJump, 05f);
+                
+            }
+        }
+
+
+
+
+
     }
     /// <summary>
     /// 滑行
     /// </summary>
     private void Slide()
     {
-        bool down = Input.GetKeyDown(KeyCode.DownArrow);
-        
-        if (down == true)
+        bool down = Input.GetKey(KeyCode.DownArrow);
+        ani.SetBool("side switch", down);
+        if(Input.GetKeyDown(KeyCode.DownArrow))  aud.PlayOneShot(soundSlide, 05f);
+            
+        if (down)
         {
-            ani.SetBool("side switch", down);
+            cap.offset = new Vector2(-0.05f, -0.5f);
+            cap.size = new Vector2(1f, 2f);
+            
+
         }
         else
         {
-
+            cap.offset = new Vector2(-0f, -0f);
+            cap.size = new Vector2(2f, 4f);
         }
-
         //X-0.05 Y-0.07 Size X 2 y 3.8
 
     }
@@ -99,6 +130,16 @@ public class Player : MonoBehaviour
     {
         Jump();
         Slide();
+        Move();
+    }
+    //繪製圖示事件:僅在Scene看得到
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        //transform.up上方       Y
+        //transform.right右方    X
+        //transform.forward前方  Z
+        Gizmos.DrawRay(transform.position + new Vector3(0, -0.8f), -transform.up * 0.01f);
     }
     #endregion
 }
